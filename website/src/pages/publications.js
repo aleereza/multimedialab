a9949df09cd866c8640ec2f2e0f7182f7dc0ce49
alireza_inspiron <alireza.barkhordari@gmail.com>
2019-04-13 19:27:46 -0700

    publications page algorithm improved, now it separates publications by year automatically

--- website/src/pages/publications.js
+++ website/src/pages/publications.js
@@ -7,59 +7,36 @@ import { graphql } from "gatsby"
 
 class PublicationsPage extends React.Component {
 
+  find_pubs(year){
+    const publicationsdata = this.props.data.allPublications.edges
+    var year_publications = publicationsdata.filter(function (row) {
+      return row.node.year == year;
+    });
+    return(year_publications)
+  }
+
   render() {
-    // const publicationsdata = this.props.data.allPublications.edges
-    const publicationsdata_2019 = this.props.data.pub2019.edges
-    const publicationsdata_2018 = this.props.data.pub2018.edges
-    const publicationsdata_2017 = this.props.data.pub2017.edges
-    const publicationsdata_2016 = this.props.data.pub2016.edges
-    const publicationsdata_2015 = this.props.data.pub2015.edges
-    const publicationsdata_2014 = this.props.data.pub2014.edges
-    const publicationsdata_2013 = this.props.data.pub2013.edges
+    const publicationsdata = this.props.data.allPublications.edges
+
+    //find unique years in publications
+    let years = [...new Set(publicationsdata.map(row => row.node.year))]
+    //convert set to array to work with map function
+    years = Array.from(years[0])
 
     return(
         <Layout>
           <div>
 
-			      <PubItemGroup
-              year={"2019"}
-              pubdata={publicationsdata_2019}
-            />
-
-            <PubItemGroup
-              year={"2018"}
-              pubdata={publicationsdata_2018}
-            />
-
-            <PubItemGroup
-              year={"2017"}
-              pubdata={publicationsdata_2017}
-            />
-
-            <PubItemGroup
-              year={"2016"}
-              pubdata={publicationsdata_2016}
-            />
-
-            <PubItemGroup
-              year={"2015"}
-              pubdata={publicationsdata_2015}
-            />
-
-            <PubItemGroup
-              year={"2014"}
-              pubdata={publicationsdata_2014}
-            />
-
-            <PubItemGroup
-              year={"2013"}
-              pubdata={publicationsdata_2013}
-            />
+            {years.map((year) => (
+              <PubItemGroup key={year}
+                year={year}
+                pubdata={this.find_pubs(year)}
+              />
+            ))}
 
             <p>For earlier publications, please refer to individual professor’s webpage.</p>
 
           </div>
-
         </Layout>
       )
 
@@ -70,7 +47,7 @@ export default PublicationsPage
 
 // in data>publications:
 //type: type of poblication: j (journal), c (conference), b (book)
-//research: index number of the reaserch wich the publication related to
+//research: index number of the reaserch which the publication related to
 export const publicationsQuery = graphql`
 query publicationsQuery {
   allPublications: allPublicationsCsv(
@@ -90,160 +67,6 @@ query publicationsQuery {
         link2
 	link1_name
 	link2_name
-      }
-    }
-  }
-
-  pub2019: allPublicationsCsv(
-    filter: { year: { eq: "2019" } },
-    sort: {fields: [index], order: ASC},
-  ){
-    edges {
-      node{
-        index
-        authors
-        title
-        reference
-	reference_detail
-        month
-        year
-        type
-        link1
-        link2
-	link1_name
-        link2_name
-      }
-    }
-  }
-
-  pub2018: allPublicationsCsv(
-    filter: { year: { eq: "2018" } },
-    sort: {fields: [index], order: ASC},
-  ){
-    edges {
-      node{
-        index
-        authors
-        title
-        reference
-	reference_detail
-        month
-        year
-        type
-        link1
-        link2
-	link1_name
-        link2_name
-      }
-    }
-  }
-
-  pub2017: allPublicationsCsv(
-    filter: { year: { eq: "2017" } },
-    sort: {fields: [index], order: ASC},
-  ){
-    edges {
-      node{
-        index
-        authors
-        title
-        reference
-	reference_detail
-        month
-        year
-        type
-        link1
-        link2
-	link1_name
-        link2_name
-      }
-    }
-  }
-
-  pub2016: allPublicationsCsv(
-    filter: { year: { eq: "2016" } },
-    sort: {fields: [index], order: ASC},
-  ){
-    edges {
-      node{
-        index
-        authors
-        title
-        reference
-	reference_detail
-        month
-        year
-        type
-        link1
-        link2
-	link1_name
-        link2_name
-      }
-    }
-  }
-
-  pub2015: allPublicationsCsv(
-    filter: { year: { eq: "2015" } },
-    sort: {fields: [index], order: ASC},
-  ){
-    edges {
-      node{
-        index
-        authors
-        title
-        reference
-	reference_detail
-        month
-        year
-        type
-        link1
-        link2
-	link1_name
-        link2_name
-      }
-    }
-  }
-
-  pub2014: allPublicationsCsv(
-    filter: { year: { eq: "2014" } },
-    sort: {fields: [index], order: ASC},
-  ){
-    edges {
-      node{
-        index
-        authors
-        title
-        reference
-	reference_detail
-        month
-        year
-        type
-        link1
-        link2
-	link1_name
-        link2_name
-      }
-    }
-  }
-
-  pub2013: allPublicationsCsv(
-    filter: { year: { eq: "2013" } },
-    sort: {fields: [index], order: ASC},
-  ){
-    edges {
-      node{
-        index
-        authors
-        title
-        reference
-	reference_detail
-        month
-        year
-        type
-        link1
-        link2
-	link1_name
-        link2_name
       }
     }
   }
