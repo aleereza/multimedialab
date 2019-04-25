50e2f8be2848acf79c2219e3827868240bad8be9
Ivan-Bajic-SFU <ibajic@ensc.sfu.ca>
2019-04-24 22:47:50 -0700

    added favicon

--- website/src/components/Layout/Layout.js
+++ website/src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Helmet } from "react-helmet"
 import Header from "../Header/Header"
 import Content from "../Content/Content"
+import favicon from "../../images/icons/m_for_ml_32x32.png"
 
 class Layout extends React.Component {
 
@@ -12,6 +13,7 @@ class Layout extends React.Component {
         <Helmet>
           <meta charSet="utf-8" />
           <title>SFU Multimedia Lab</title>
+		  <link rel="icon" type="image/png" href={favicon} />
         </Helmet>
         <Header/>
         <Content>
